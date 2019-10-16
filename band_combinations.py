eee1710a22c88662bc06e50603bd713cbe83fd59
Sean Kwek <seankwekjk@gmail.com>
2019-10-16 14:01:19 +0800

    update to use default naming conventions

--- band_combinations.py
+++ band_combinations.py
@@ -1,3 +1,4 @@
+import os
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,10 +13,13 @@ def normalize(array):
 
 
 # Open the file:
-parent_folder = "D:/SatelliteData/Aug-Oct_2019_Indonesia/Landsat8_30092019/LC08_L1TP_127060_20190930_20191001_01_RT/"
-red_tif = rasterio.open(parent_folder + 'band7.TIF')
-green_tif = rasterio.open(parent_folder + 'band5.TIF')
-blue_tif = rasterio.open(parent_folder + 'band2.TIF')
+parent_folder = "D:/SatelliteData/Aug-Oct_2019_Indonesia/Landsat8_30092019/LC08_L1TP_127060_20190930_20191001_01_RT"
+image_list = os.listdir(parent_folder)
+# print(image_list)
+red_tif = rasterio.open(parent_folder + '/' + image_list[9])
+green_tif = rasterio.open(parent_folder + '/' + image_list[7])
+blue_tif = rasterio.open(parent_folder + '/' + image_list[4])
+ir_tif = rasterio.open(parent_folder + '/' + image_list[12])
 
 # Convert to numpy arrays
 red = red_tif.read(1)
@@ -24,6 +28,8 @@ green = green_tif.read(1)
 # show(green)
 blue = blue_tif.read(1)
 # show(blue)
+ir = ir_tif.read(1)
+# show(ir)
 
 # Normalize band DN
 redn = normalize(red)
