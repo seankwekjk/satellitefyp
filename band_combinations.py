b07aee83b440ccd1ece38134a539fd403a3ca749
Sean Kwek <seankwekjk@gmail.com>
2019-10-30 21:17:37 +0800

    update to automate multiple folders

--- band_combinations.py
+++ band_combinations.py
@@ -13,32 +13,35 @@ def normalize(array):
 
 
 # Open the file:
-parent_folder = "D:/SatelliteData/Aug-Oct_2019_Indonesia/Landsat8_30092019/LC08_L1TP_127060_20190930_20191001_01_RT"
-image_list = os.listdir(parent_folder)
-# print(image_list)
-red_tif = rasterio.open(parent_folder + '/' + image_list[9])
-green_tif = rasterio.open(parent_folder + '/' + image_list[7])
-blue_tif = rasterio.open(parent_folder + '/' + image_list[4])
-ir_tif = rasterio.open(parent_folder + '/' + image_list[12])
+outer_folder = "D:/SatelliteData/Aug-Oct_2019_Indonesia/landsat"
+folder_list = os.listdir(outer_folder)
+for folder in folder_list:
+    parent_folder = outer_folder + '/' + folder
+    image_list = os.listdir(parent_folder)
+    # print(image_list)
+    red_tif = rasterio.open(parent_folder + '/' + image_list[9])
+    green_tif = rasterio.open(parent_folder + '/' + image_list[7])
+    blue_tif = rasterio.open(parent_folder + '/' + image_list[4])
+    ir_tif = rasterio.open(parent_folder + '/' + image_list[12])
 
-# Convert to numpy arrays
-red = red_tif.read(1)
-# show(red)
-green = green_tif.read(1)
-# show(green)
-blue = blue_tif.read(1)
-# show(blue)
-ir = ir_tif.read(1)
-# show(ir)
+    # Convert to numpy arrays
+    red = red_tif.read(1)
+    # show(red)
+    green = green_tif.read(1)
+    # show(green)
+    blue = blue_tif.read(1)
+    # show(blue)
+    ir = ir_tif.read(1)
+    # show(ir)
 
-# Normalize band DN
-redn = normalize(red)
-bluen = normalize(blue)
-greenn = normalize(green)
+    # Normalize band DN
+    redn = normalize(red)
+    bluen = normalize(blue)
+    greenn = normalize(green)
 
-# Stack bands
-rgb = np.dstack((redn, greenn, bluen))
+    # Stack bands
+    rgb = np.dstack((redn, greenn, bluen))
 
-# View the color composite
-plt.imshow(rgb)
-plt.show()
+    # View the color composite
+    plt.imsave('img/' + folder + '.png', rgb)
+    plt.imsave('ir_img/' + folder + '.png', ir)
